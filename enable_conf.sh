6f98cae8cf58d572ce22c09d496253d76f8aa612
Daniel Porto <daniel.porto@gmail.com>
2019-06-18 21:35:51 +0100

    Adding support for running a local docker-compose deployment of ycsb in-memory db

--- enable_conf.sh
+++ enable_conf.sh
@@ -46,8 +46,11 @@ if [ ${c} == 'ycsb' -a ${l} == 'single' -a ${d} == 'yes'  ]; then
     echo "Configuration written."
 elif [ ${c} == 'ycsb' -a ${l} == 'single'  -a ${d} == 'no'  ]; then
     echo "ycsb, single and memory."
-    # cp dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
-    echo "Not yet implemented."
+    rm docker-compose.yml
+    ln -s dockerfiles/docker-compose-ycsb.yml docker-compose.yml
+    rm config/system.config
+    cp config/system.config.default config/system.config
+    echo "Configuration written."
 
 elif [ ${c} == 'ycsb' -a ${l} == 'multi'  -a ${d} == 'yes'  ]; then
     echo "ycsb, multi and durable."

6f7d034abbb6a32de5232c0d8b66ef9f709f1a49
Daniel Porto <daniel.porto@gmail.com>
2019-07-11 11:53:03 +0100

    Configuration enabler was refactored and included microbenchmark

--- enable_conf.sh
+++ enable_conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() { 
-    echo "Usage: $0 [-c <ycsb|counter>] [-l <single|multi>] [-d <yes|no>]" 1>&2; 
+    echo "Usage: $0 [-c <ycsb|counter|bench>] [-l <single|multi>] [-d <yes|no>]" 1>&2; 
     echo "Where 'c' is a short for configuration, 'l' for type of loader and 'd' indicates if durable or not."
     exit 1; 
 }
@@ -10,7 +10,7 @@ while getopts ":c:l:d:" o; do
     case "${o}" in
         c)
             c=${OPTARG}
-            ((c == 'ycsb' || c == 'counter')) || usage
+            ((c == 'ycsb' || c == 'counter' || c == 'bench' )) || usage
             ;;
         l)
             l=${OPTARG}
@@ -36,56 +36,104 @@ fi
 # echo "s = ${c}"
 # echo "p = ${l}"
 # echo "d = ${d}"
+cleanup() {
+    rm -f docker-compose.yml
+    rm -f config/system.config
+    rm -f config/currentView
+    echo "Configuration clean."
+}
 
-if [ ${c} == 'ycsb' -a ${l} == 'single' -a ${d} == 'yes'  ]; then
-    echo "ycsb, single and durable."
-    rm docker-compose.yml
-    ln -s dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
-    rm config/system.config
-    cp config/system.config.durable config/system.config
+
+configure_ycsb() {
+    cleanup;
+    echo "Configuring YCSB deployment: $1, durable: $2"
+
+    if [ ${1} == 'single' -a ${2} == 'yes'  ]; then
+        echo "ycsb, single and durable."
+        ln -s dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
+        cp config/system.config.durable config/system.config
+    elif [ ${1} == 'single'  -a ${2} == 'no'  ]; then
+        echo "ycsb, single and memory."
+        ln -s dockerfiles/docker-compose-ycsb.yml docker-compose.yml
+        cp config/system.config.default config/system.config
+    elif [ {$1} == 'multi'  -a ${2} == 'yes'  ]; then
+        echo "ycsb, multi and durable."
+        echo "Not yet implemented."
+    elif [ {$1} == 'multi'  -a ${2} == 'no'  ]; then
+        echo "ycsb, multi and memory."
+        echo "Not yet implemented."
+        exit;
+    fi    
+    
     echo "Configuration written."
-elif [ ${c} == 'ycsb' -a ${l} == 'single'  -a ${d} == 'no'  ]; then
-    echo "ycsb, single and memory."
-    rm docker-compose.yml
-    ln -s dockerfiles/docker-compose-ycsb.yml docker-compose.yml
-    rm config/system.config
-    cp config/system.config.default config/system.config
+}
+
+configure_counter() {
+    cleanup;
+    echo "Configuring counter deployment: $1, durable: $2"
+
+    if [ ${1} == 'single' -a ${2} == 'yes'  ]; then
+        echo "counter, single and durable."
+        echo "Not yet implemented."
+        exit;
+    elif [ ${1} == 'single'  -a ${2} == 'no'  ]; then
+        echo "counter, single and memory."
+        ln -s dockerfiles/docker-compose-counter.yml docker-compose.yml
+        cp config/system.config.default config/system.config
+    elif [ ${1} == 'multi'  -a ${2} == 'yes'  ]; then
+        echo "counter, multi and durable."
+        echo "Not yet implemented."
+        exit;
+    elif [ ${1} == 'multi'  -a ${2} == 'no'  ]; then
+        echo "counter, multi and memory."
+        echo "Not yet implemented."
+        exit;
+    fi
+
     echo "Configuration written."
+}
 
-elif [ ${c} == 'ycsb' -a ${l} == 'multi'  -a ${d} == 'yes'  ]; then
-    echo "ycsb, multi and durable."
-    # cp dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
-#    rm config/system.config
-#    cp config/system.config.durable config/system.config
-    echo "Not yet implemented."
+configure_benchmark() {
+    cleanup;
+    echo "Configuring microbenchmark deployment: $1, durable: $2"
 
-elif [ ${c} == 'ycsb' -a ${l} == 'multi'  -a ${d} == 'no'  ]; then
-    echo "ycsb, multi and memory."
-    # cp dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
-    echo "Not yet implemented."
+    if [ ${1} == 'single' -a ${2} == 'yes'  ]; then
+        echo "microbenchmark, single and durable."
+        echo "Not yet implemented."
+        exit;
+    elif [ ${1} == 'single'  -a ${2} == 'no'  ]; then
+        echo "microbenchmark, single and memory."
+        ln -s dockerfiles/docker-compose-microbenchmark.yml docker-compose.yml
+        cp config/system.config.default config/system.config
+    elif [ ${1} == 'multi'  -a ${2} == 'yes'  ]; then
+        echo "microbenchmark, multi and durable."
+        echo "Not yet implemented."
+        exit;
+    elif [ ${1} == 'multi'  -a ${2} == 'no'  ]; then
+        echo "microbenchmark, multi and memory."
+        echo "Not yet implemented."
+        exit;
+    fi
 
-elif [ ${c} == 'counter' -a ${l} == 'single' -a ${d} == 'yes'  ]; then
-    echo "counter, single and durable."
-    # cp dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
-#    rm config/system.config
-#    cp config/system.config.durable config/system.config
-    echo "Not yet implemented."
-
-elif [ ${c} == 'counter' -a ${l} == 'single'  -a ${d} == 'no'  ]; then
-    echo "counter, single and memory."
-    rm docker-compose.yml
-    ln -s dockerfiles/docker-compose-counter.yml docker-compose.yml
-    rm config/system.config
-    cp config/system.config.default config/system.config
     echo "Configuration written."
-elif [ ${c} == 'counter' -a ${l} == 'multi'  -a ${d} == 'yes'  ]; then
-    echo "counter, multi and durable."
-    # cp dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
-#    rm config/system.config
-#    cp config/system.config.durable config/system.config
-    echo "Not yet implemented."
-elif [ ${c} == 'counter' -a ${l} == 'multi'  -a ${d} == 'no'  ]; then
-    echo "counter, multi and memory."
-    # cp dockerfiles/docker-compose-ycsb-durable.yml docker-compose.yml
-    echo "Not yet implemented."
-fi;
+}
+
+
+
+
+
+
+case "${c}" in
+    ycsb)
+        configure_ycsb ${l} ${d}
+        ;;
+    counter)
+        configure_counter ${l} ${d}
+        ;;
+    bench)
+        configure_benchmark ${l} ${d}
+        ;;
+    *)
+        usage
+        ;;
+esac
